bf7d55dbf4e48cf7e2778b07b3b88d9ca349abe6
Connor Cox <10873383@my.uvu.edu>
2020-01-23 15:35:33 -0700

    AnimController

---
+++ Character Scripts/Assets/Scripts/AnimBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimBehavior : MonoBehaviour
+{
+    public UnityEvent stateEnterEvent;
+    
+    override public void
+}
