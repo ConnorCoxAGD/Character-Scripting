e6636e0ef0eee6503dd1c48be5bd971d79ab7af2
Connor Cox <10873383@my.uvu.edu>
2020-01-24 13:36:15 -0700

    Understanding Character Movement

--- Character Scripts/Assets/Scripts/AnimBehavior.cs
+++ Character Scripts/Assets/Scripts/AnimBehavior.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class AnimBehavior : MonoBehaviour
+public class AnimBehaviour : StateMachineBehaviour
 {
     public UnityEvent stateEnterEvent;
 
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateEnterEvent.Invoke();
+    }
 }
