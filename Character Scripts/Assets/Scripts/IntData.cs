90f96dcbf1dad687d25945d8d40284057cc61f9e
Connor Cox <10873383@my.uvu.edu>
2020-03-05 14:56:13 -0700

    Dialogue and UI Systems

---
+++ Character Scripts/Assets/Scripts/IntData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenuAttribute]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void SetValue(int amount)
+    {
+        value = amount;
+    }
+
+    public void UpdateValue(int amount)
+    {
+        value += amount;
+    }
+    
+}
---
+++ Character Scripts/Assets/Scripts/TextBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    private Text textObj;
+
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    public void UpdateText(StringListData stringListDataObj)
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+    public void UpdateText(IntData intDataObj)
+    {
+        textObj.text = intDataObj.value.ToString();
+    }
+}
