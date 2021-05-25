48303b0322cf18cbe61131cc8ae95f8fd8e2ad3a
Connor Cox <connorcoxr@gmail.com>
2021-05-25 14:38:34 -0600

    FPS changes

--- Character Scripts/Assets/Scripts/Movement/FPSMovement.cs
+++ Character Scripts/Assets/Scripts/Movement/FPSMovement.cs
@@ -3,19 +3,33 @@ using UnityEngine;
 public class FPSMovement : MonoBehaviour
 {
     
-    public float xSpeed, zSpeed, gravity;
-    private CharacterController _controller;
-    private Vector3 _position;
+    public float speed, jumpForce;
+    private CharacterController controller;
+    private Vector3 moveVector = Vector3.zero;
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        _position.Set(Input.GetAxis("Horizontal") * xSpeed, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * zSpeed);
-      
-        _position = _controller.transform.TransformDirection(_position);
-        _controller.Move(_position * Time.deltaTime);
+        //sets movement vector
+        
+        moveVector.Set(Input.GetAxis("Horizontal") * speed, moveVector.y, Input.GetAxis("Vertical") * speed);
+        moveVector = controller.transform.TransformDirection(moveVector);
+        //adds gravity
+        if(!controller.isGrounded){
+            moveVector.y = Physics.gravity.y;
+        }
+        //adds Jump
+        if(controller.isGrounded && Input.GetKeyDown(KeyCode.Space)){
+            moveVector.y = jumpForce;
+            print(moveVector.y);
+        }
+
+        print(moveVector);
+
+
+        controller.Move(moveVector * Time.deltaTime);
     }
 }
