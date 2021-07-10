e0403599f209af18966a26e0befde76e4eee313f
Connor Cox <connorcoxr@gmail.com>
2021-07-09 23:25:10 -0600

    FPS Controller feautres working jump with limited air control

--- Character Scripts/Assets/Scripts/Movement/FPSMovement.cs
+++ Character Scripts/Assets/Scripts/Movement/FPSMovement.cs
@@ -2,34 +2,58 @@ using UnityEngine;
 
 public class FPSMovement : MonoBehaviour
 {
-    
-    public float speed, jumpForce;
-    private CharacterController controller;
-    private Vector3 moveVector = Vector3.zero;
-    private void Start()
-    {
-        controller = GetComponent<CharacterController>();
+    public float walkingSpeed = 10, 
+        runningSpeed = 20, 
+        jumpForce = 15,
+        groundDistance = -.4f;
+    public Transform groundCheck;
+
+    private CharacterController cc;
+    private Vector3 verticalVelocity, previousMoveVector;
+    private bool isGrounded;
+    private float currentSpeed;
+
+    private void Start(){
+        cc = GetComponent<CharacterController>();
     }
 
-    private void Update()
-    {
-        //sets movement vector
+    private void Update(){
+        //checks if controller is grounded more accurately than CharacterController
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
         
-        moveVector.Set(Input.GetAxis("Horizontal") * speed, moveVector.y, Input.GetAxis("Vertical") * speed);
-        moveVector = controller.transform.TransformDirection(moveVector);
-        //adds gravity
-        if(!controller.isGrounded){
-            moveVector.y = Physics.gravity.y;
+        //resets fall speed while grounded
+        if (isGrounded && verticalVelocity.y < 0){
+            verticalVelocity.y = 0f;
         }
-        //adds Jump
-        if(controller.isGrounded && Input.GetKeyDown(KeyCode.Space)){
-            moveVector.y = jumpForce;
-            print(moveVector.y);
+        Vector3 movement = transform.right * x + transform.forward * z;
+
+        //allows sprint
+        if(Input.GetKey(KeyCode.LeftShift) && !isGrounded){
+            currentSpeed = runningSpeed;
+        }
+        else{
+            currentSpeed = walkingSpeed;
         }
 
-        print(moveVector);
+        if(!isGrounded){
+            previousMoveVector = Vector3.MoveTowards(previousMoveVector, movement, 1.5f * Time.deltaTime);
+            cc.Move(previousMoveVector * currentSpeed * Time.deltaTime);
+        }
+        else{
+            previousMoveVector = movement;
+            cc.Move(movement * currentSpeed * Time.deltaTime);
+        }
+        
 
+        //jump
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            verticalVelocity.y += Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y);
+            print("jump");
+        }
 
-        controller.Move(moveVector * Time.deltaTime);
+        verticalVelocity.y += Physics.gravity.y * Time.deltaTime;
+        cc.Move(verticalVelocity * Time.deltaTime);
     }
 }
